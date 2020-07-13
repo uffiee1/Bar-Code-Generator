9abad5354462e7c19f9ffa258f48564e2a36dafa
Ufuk Angay <u.angay@gmail.com>
2020-07-14 01:46:26 +0200

    Updated icon-Final

--- Bar Code Generator/Form1.Designer.cs
+++ Bar Code Generator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.txbxGenerate = new System.Windows.Forms.TextBox();
@@ -80,8 +81,9 @@
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txbxGenerate);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BarCode Generator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
